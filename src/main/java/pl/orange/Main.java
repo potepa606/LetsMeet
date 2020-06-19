1980c76ddd2ec71ddc9ea0f6c4d4b8143ba23944
potepa606 <34142069+potepa606@users.noreply.github.com>
2020-06-19 03:15:53 +0200

    finally release

--- src/main/java/pl/orange/Main.java
+++ src/main/java/pl/orange/Main.java
@@ -1,20 +1,16 @@
 package pl.orange;
 
-import org.json.*;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
+
 import pl.orange.initializer.CalendarsGenerator;
-import pl.orange.jsonParser.JSONReader;
 import pl.orange.models.Calendar;
 import pl.orange.setMeet.SetMeeting;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.time.Duration;
+import java.time.LocalTime;
+import java.time.Period;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
 
@@ -26,19 +22,32 @@ public class Main {
         CalendarsGenerator calendarsGenerator = new CalendarsGenerator(paths);
         ArrayList<Calendar> calendars = calendarsGenerator.createCalendars();
 
-        for (Calendar calendar : calendars) {
-         //   System.out.println(calendar);
-        }
-
         Calendar calendar1 = calendars.get(0);
         Calendar calendar2 = calendars.get(1);
 
+        ArrayList<LocalTime[]> godziny = new ArrayList<>();
+
         SetMeeting sm = new SetMeeting();
-        sm.setMeeting(calendar1,calendar2);
+        ArrayList<String[]> hours  = sm.setMeeting(calendar1,calendar2);
+        for (String[] hour : hours) {
+            LocalTime[] localTimes = new LocalTime[2];
+            localTimes[0] = LocalTime.parse(hour[0]);
+            localTimes[1] = LocalTime.parse(hour[hour.length-1]);
+            godziny.add(localTimes);
+        }
 
+        Scanner duration = new Scanner(System.in);
+        System.out.println("Podaj przewidywany czas spotkania - w minutach!");
+        long zadanyCzasMinutach = duration.nextInt();
 
+        for (LocalTime[] godz : godziny) {
+            long czasWsekundach = Duration.between(godz[0],godz[1]).getSeconds();
+            long czasWminutach = czasWsekundach/60;
 
-
+            if(czasWminutach > zadanyCzasMinutach ){
+                System.out.print(Arrays.toString(godz) + " ");
+            }
+        }
     }
 
 }
--- src/main/java/pl/orange/setMeet/SetMeeting.java
+++ src/main/java/pl/orange/setMeet/SetMeeting.java
@@ -14,16 +14,21 @@ public class SetMeeting {
     static LocalTime[] cal1 = new LocalTime[48]; // kazda komorka to plus 30 min. od 00:00 do 23:30
     static LocalTime[] cal2 = new LocalTime[48];
 
+    static ArrayList<LocalTime> freeTime = new ArrayList<>();
+    static ArrayList<LocalTime> freeTime2 = new ArrayList<>();
+
+    static ArrayList<String[]> finallyFreeTime = new ArrayList<>();
 
 
-    public void setMeeting(Calendar calendar1, Calendar calendar2){
+
+    public ArrayList<String[]> setMeeting(Calendar calendar1, Calendar calendar2) {
 
         generateCalendarArrays();
         for (int i = 0; i < 48; i++) {
-            if(cal1[i] != null && ( cal1[i].isBefore(calendar1.getWorkingStart()) || cal1[i].isAfter(calendar1.getWorkingEnd())) ){
+            if (cal1[i] != null && (cal1[i].isBefore(calendar1.getWorkingStart()) || cal1[i].isAfter(calendar1.getWorkingEnd()))) {
                 cal1[i] = null;
             }
-            if(cal2[i] != null && ( cal2[i].isBefore(calendar2.getWorkingStart()) || cal2[i].isAfter(calendar2.getWorkingEnd())) ){
+            if (cal2[i] != null && (cal2[i].isBefore(calendar2.getWorkingStart()) || cal2[i].isAfter(calendar2.getWorkingEnd()))) {
                 cal2[i] = null;
             }
         }
@@ -32,50 +37,102 @@ public class SetMeeting {
         ArrayList<Meet> plannedMeetings_Person_2 = calendar2.getPlannedMeeting();
 
 
-        for (Meet pm : plannedMeetings_Person_1) {
-            LocalTime startMeet_Person_1 = pm.getStart();
-            LocalTime endMeet_Person_1 = pm.getEnd();
+        setNullMeetingPositions(plannedMeetings_Person_1, cal1, calendar1);
+        setNullMeetingPositions(plannedMeetings_Person_2, cal2, calendar2);
 
-            for (int i = 0; i < 48; i++) {
-                if(cal1[i] != null && ( (cal1[i].equals(startMeet_Person_1) | cal1[i].equals(endMeet_Person_1)) || ( cal1[i].isAfter(startMeet_Person_1) && cal1[i].isBefore(endMeet_Person_1) ))){
-                    cal1[i] = null;
-                }
-            }
-        }
-        for (Meet pm : plannedMeetings_Person_2) {
-            LocalTime startMeet_Person_2 = pm.getStart();
-            LocalTime  endMeet_Person_2 = pm.getEnd();
+        return checkFreeTime();
 
-            for (int i = 0; i < 48; i++) {
-                if(cal2[i] != null && ( (cal2[i].equals(startMeet_Person_2) | cal2[i].equals(endMeet_Person_2)) || ( cal2[i].isAfter(startMeet_Person_2) && cal2[i].isBefore(endMeet_Person_2) ))){
-                    cal2[i] = null;
-                }
-            }
-        }
-
-
-        display();
+//        display();
     }
 
-    private static void generateCalendarArrays(){
-        cal1[0] = LocalTime.of(0,0);
+
+    private ArrayList<String[]> checkFreeTime() {
+        boolean save = true;
+        for (int i = 1; i < 47; i++) {
+            if(cal1[i] != null && cal2[i] != null && cal1[i].equals(cal2[i])){
+                freeTime.add(cal1[i]);
+            }
+        }
+
+        for (int i = 0; i < freeTime.size()-1; i++) {
+            LocalTime time = freeTime.get(i).plusMinutes(30);
+            if(time.equals(freeTime.get(i+1))) {
+                freeTime2.add(freeTime.get(i));
+               // freeTime2.add(time);
+            }else if(freeTime.get(i).equals(freeTime.get(i-1).plusMinutes(30))){
+                freeTime2.add(freeTime.get(i));
+
+            }
+            if(!time.equals(freeTime.get(i+1))) {
+                freeTime2.add(null);
+            }
+        }
+
+        LocalTime[] arrTimes = new LocalTime[2];
+        //freeTime2.add(0, null);
+
+        String test = "";
+        for (LocalTime localTime : freeTime2) {
+            test += localTime + "";
+        }
+        String[] test2 = test.split("null");
+        for (int i = 0; i < test2.length; i++) {
+            finallyFreeTime.add(test2[i].split("(?<=\\G.{5})"));
+        }
+
+        for (String[] strings : finallyFreeTime) {
+        //    System.out.println(Arrays.toString(strings));
+        }
+
+       // freeTime2.forEach(t -> System.out.println(t));
+
+        return finallyFreeTime;
+    }
+
+    private void setNullMeetingPositions(ArrayList<Meet> plannedMeetings_Person, LocalTime[] cal, Calendar calendar) {
+        LocalTime endMeet_Person_Prev = null;
+
+
+        for (Meet pm : plannedMeetings_Person) {
+            LocalTime startMeet_Person = pm.getStart();
+            LocalTime endMeet_Person = pm.getEnd();
+
+
+            for (int i = 0; i < 48; i++) {
+                if (cal[i] != null && ((cal[i].isAfter(startMeet_Person) && cal[i].isBefore(endMeet_Person)))) {
+                    cal[i] = null;
+                }
+                if (endMeet_Person_Prev != null && cal[i] != null && cal[i].equals(startMeet_Person) && (endMeet_Person_Prev.equals(startMeet_Person))) {
+                    cal[i] = null;
+                }
+                if (cal[i] != null && cal[i].equals(startMeet_Person) && startMeet_Person.equals(calendar.getWorkingStart())) {
+                    cal[i] = null;
+                }
+
+            }
+            endMeet_Person_Prev = endMeet_Person;
+        }
+    }
+
+    private static void generateCalendarArrays() {
+        cal1[0] = LocalTime.of(0, 0);
         for (int i = 1; i < cal1.length; i++) {
-            LocalTime previousTimePlus30minutes = cal1[i-1].plusMinutes(30);
+            LocalTime previousTimePlus30minutes = cal1[i - 1].plusMinutes(30);
             cal1[i] = previousTimePlus30minutes;
         }
         // Arrays.stream(cal1).forEach(time -> System.out.println(time));
 
-        cal2[0] = LocalTime.of(0,0);
+        cal2[0] = LocalTime.of(0, 0);
         for (int i = 1; i < cal2.length; i++) {
-            LocalTime previousTimePlus30minutes = cal2[i-1].plusMinutes(30);
+            LocalTime previousTimePlus30minutes = cal2[i - 1].plusMinutes(30);
             cal2[i] = previousTimePlus30minutes;
         }
         // Arrays.stream(cal1).forEach(time -> System.out.println(time));
 
     }
 
-    private static void display(){
-        for(int i=0; i<48 ;i++){
+    private static void display() {
+        for (int i = 0; i < 48; i++) {
             System.out.println(cal1[i] + " - - " + cal2[i]);
         }
 
